bb4e4a1562c93dec8a40128213d2ed4d3a1acf95
Alem Tuzlak <t.zlak@hotmail.com>
2025-08-19 10:53:05 +0200

    feat: add new components (tag, button) and improve JsonTree

---
+++ packages/devtools-ui/src/components/tag.tsx
@@ -0,0 +1,57 @@
+import { Show, createSignal, } from "solid-js"
+import { useStyles } from "../styles/use-styles"
+import type { tokens } from "../styles/tokens";
+
+export const Tag = (props: {
+  color: keyof typeof tokens.colors;
+  label: string;
+  count?: number;
+  disabled?: boolean
+}) => {
+  const styles = useStyles()
+
+  let tagRef!: HTMLButtonElement
+
+  const [mouseOver, setMouseOver] = createSignal(false)
+  const [focused, setFocused] = createSignal(false)
+
+
+  return (
+    <button
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => {
+        setMouseOver(false)
+        setFocused(false)
+      }}
+      disabled={props.disabled}
+      ref={tagRef}
+      class={styles().tag.queryStatusTag}
+      {...(mouseOver() || focused()
+        ? {
+          'aria-describedby': 'tsqd-status-tooltip',
+        }
+        : {})}
+    >
+
+      <span
+        class={styles().tag.dot(props.color)}
+      />
+      <span
+        class={styles().tag.queryStatusTagLabel}
+      >
+        {props.label}
+      </span>
+
+
+      <Show when={props.count && props.count > 0}>
+        <span
+          class={styles().tag.queryStatusCount}
+        >
+          {props.count}
+        </span>
+      </Show>
+    </button>
+  )
+}
