07478df8a4415e0abcf0d08f42e6deaf546663bc
Alem Tuzlak <t.zlak@hotmail.com>
2025-08-19 08:19:37 +0200

    chore: fix tests

--- packages/event-bus-client/src/index.ts
+++ packages/event-bus-client/src/index.ts
@@ -1,4 +1,22 @@
 import * as Client from './plugin'
 
-export const EventClient =
-  process.env.NODE_ENV !== 'development' ? class {} : Client.EventClient
+class MockClass {
+  on(_: string, __: (...args: Array<any>) => void) {
+    // No-op in production
+  }
+  onAll(_: (...args: Array<any>) => void) {
+    // No-op in production
+  }
+  onAllPluginEvents(_: (...args: Array<any>) => void) {
+    // No-op in production
+  }
+  emit(_: string, ...__: Array<any>) {
+    // No-op in production
+  }
+  getPluginId() {
+    // No-op in production
+  }
+}
+
+export const EventClient: typeof Client["EventClient"] =
+  process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== "test" ? MockClass as any : Client.EventClient
