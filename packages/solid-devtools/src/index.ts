92b5c9a5e90750ae97c87a379ab8a0df33d19003
Alem Tuzlak <t.zlak@hotmail.com>
2025-08-20 14:43:40 +0200

    test

--- packages/solid-devtools/src/index.ts
+++ packages/solid-devtools/src/index.ts
@@ -1,18 +1,18 @@
 import { isDev } from 'solid-js/web'
 import * as Devtools from './devtools'
-import type { CustomElements } from '@tanstack/devtools-ui/types'
+import type { CustomElements } from '@tanstack/devtools-ui'
 
 declare module 'solid-js' {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
-    interface IntrinsicElements extends CustomElements {}
+    interface IntrinsicElements extends CustomElements { }
   }
 }
 
 export const TanStackDevtools: (typeof Devtools)['TanStackDevtools'] = isDev
   ? Devtools.TanStackDevtools
   : function () {
-      return null
-    }
+    return null
+  }
 
 export type { TanStackDevtoolsSolidPlugin } from './core'
