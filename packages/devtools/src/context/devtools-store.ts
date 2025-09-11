e88b1872b9de4ad6cc75644a41d4ee72437d58b8
Alem Tuzlak <t.zlak@hotmail.com>
2025-09-11 11:06:10 +0200

    fix: remove invalid plugins from active plugins on mount

--- packages/devtools/src/context/devtools-store.ts
+++ packages/devtools/src/context/devtools-store.ts
@@ -81,8 +81,8 @@ export const initialState: DevtoolsStore = {
     requireUrlFlag: false,
     urlFlag: 'tanstack-devtools',
     theme:
-      typeof window !== 'undefined' &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
+      typeof window !== 'undefined' && typeof window.matchMedia !== "undefined" &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
         ? 'dark'
         : 'light',
   },
