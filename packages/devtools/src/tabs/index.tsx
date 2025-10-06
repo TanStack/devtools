d8b58ed75e0f3c8e34215a34ef11dced36ef90a5
sbreiler <steffen@ftws.de>
2025-10-06 17:20:39 +0200

    Add option to enable/disable (default: enabled) standard seo-tab and settings-tab inside the devtools

--- packages/devtools/src/tabs/index.tsx
+++ packages/devtools/src/tabs/index.tsx
@@ -2,6 +2,7 @@ import { Cogs, List, PageSearch } from '@tanstack/devtools-ui/icons'
 import { SettingsTab } from './settings-tab'
 import { PluginsTab } from './plugins-tab'
 import { SeoTab } from './seo-tab'
+import type { DevtoolsStore } from '../context/devtools-store'
 
 export const tabs = [
   {
@@ -23,5 +24,13 @@ export const tabs = [
     icon: () => <Cogs />,
   },
 ] as const
+
+export const getTabs = (settings: DevtoolsStore['settings']) => {
+  return tabs.filter((t) => {
+    if (t.id === 'seo') return settings.enableSeoTab
+    if (t.id === 'settings') return settings.enableSettingsTab
+    return true
+  })
+}
 
 export type TabName = (typeof tabs)[number]['id']
