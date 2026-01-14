fc522c9c5624b004c787e1902edcfaa5e728fe71
Birk Skyum <birk.skyum@pm.me>
2026-01-14 01:17:39 +0100

    feat(devtools-utils): export vue

---
+++ packages/devtools-utils/vite.config.vue.ts
@@ -0,0 +1,25 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { tanstackViteConfig } from '@tanstack/vite-config'
+import packageJson from './package.json'
+
+const config = defineConfig({
+  plugins: [],
+  test: {
+    name: packageJson.name,
+    dir: './',
+    watch: false,
+    environment: 'jsdom',
+    setupFiles: ['./tests/test-setup.ts'],
+    globals: true,
+  },
+})
+
+export default mergeConfig(
+  config,
+  tanstackViteConfig({
+    entry: ['./src/vue/index.ts'],
+    srcDir: './src/vue',
+    outDir: './dist/vue',
+    cjs: false,
+  }),
+)
