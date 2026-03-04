1e97bd51b17fcdb6062ef5ebc9a65aade234c0ae
Alem Tuzlak <t.zlak@hotmail.com>
2026-03-04 16:57:36 +0100

    feat(devtools-utils): add Svelte factory functions and build config

---
+++ packages/devtools-utils/src/svelte/plugin.ts
@@ -0,0 +1,24 @@
+import type { Component } from 'svelte'
+
+export function createSveltePlugin(
+  name: string,
+  component: Component<any>,
+) {
+  function Plugin(props?: Record<string, any>) {
+    return {
+      name,
+      component,
+      props,
+    }
+  }
+
+  function NoOpPlugin(props?: Record<string, any>) {
+    return {
+      name,
+      component: (() => {}) as unknown as Component<any>,
+      props,
+    }
+  }
+
+  return [Plugin, NoOpPlugin] as const
+}
