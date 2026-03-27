c61ea0f5a0e3b22ce8e76906f8388134e9905bce
Alem Tuzlak <t.zlak@hotmail.com>
2026-03-27 10:54:40 +0100

    Refactor code structure for improved readability and maintainability

--- examples/react/start-cloudflare/src/routes/__root.tsx
+++ examples/react/start-cloudflare/src/routes/__root.tsx
@@ -22,6 +22,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body style={{ margin: 0, fontFamily: 'system-ui, sans-serif' }}>
         {children}
         <TanStackDevtools
+          eventBusConfig={{
+          connectToServerBus: true
+        }}
           config={{ position: 'bottom-right' }}
           plugins={[
             {
