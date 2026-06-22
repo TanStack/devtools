3ba87baab8c29679f9db44ecc44e4d1e104236fd
Alem Tuzlak <t.zlak@hotmail.com>
2026-06-22 18:34:38 +0200

    test(e2e): preact adapter smoke

---
+++ e2e/apps/preact/src/index.tsx
@@ -0,0 +1,20 @@
+import { render } from 'preact'
+import { TanStackDevtools } from '@tanstack/preact-devtools'
+
+function App() {
+  return (
+    <>
+      <h1>preact e2e</h1>
+      <TanStackDevtools
+        plugins={[
+          {
+            name: 'Demo',
+            render: () => <div data-testid="demo-plugin">demo plugin content</div>,
+          },
+        ]}
+      />
+    </>
+  )
+}
+
+render(<App />, document.getElementById('root')!)
