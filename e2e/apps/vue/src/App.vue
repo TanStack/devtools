f05d02cec56c9702ba1fad417669b2860d533ef1
Alem Tuzlak <t.zlak@hotmail.com>
2026-06-22 18:33:18 +0200

    test(e2e): vue adapter smoke

---
+++ e2e/apps/vue/src/App.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+import {
+  TanStackDevtools,
+  TanStackDevtoolsVuePlugin,
+} from '@tanstack/vue-devtools'
+
+const DemoPlugin = defineComponent({
+  name: 'DemoPlugin',
+  render() {
+    return h('div', { 'data-testid': 'demo-plugin' }, 'demo plugin content')
+  },
+})
+
+export default defineComponent({
+  name: 'App',
+  components: { TanStackDevtools },
+  setup() {
+    const plugins: TanStackDevtoolsVuePlugin[] = [
+      {
+        name: 'Demo',
+        component: DemoPlugin,
+      },
+    ]
+
+    return {
+      plugins,
+    }
+  },
+})
+</script>
+
+<template>
+  <h1>vue e2e</h1>
+  <TanStackDevtools :plugins="plugins" />
+</template>
