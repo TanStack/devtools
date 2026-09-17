cef54e3e020b85062d98d60d25480b38f76af2a7
parth-goai <parth-goai@users.noreply.github.com>
2026-09-17 17:17:03 +0530

    Integrity-verified, quality-unverified draft: remove 5 findings

--- packages/devtools-a11y/src/angular/production.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-export { A11yDevtoolsPanel } from './production/A11yDevtools'
-
-export type { A11yDevtoolsAngularInit } from './production/A11yDevtools'
-
-export { a11yDevtoolsPlugin } from './production/plugin'
